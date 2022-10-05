7ceb52cb5c0657f147e31194fd74ab2994cb626e
chiang-1221 <72018950+chiang-1221@users.noreply.github.com>
2022-10-05 15:56:09 +0800

    Delete sketch_aug18e.ino

--- automatic control/sketch_aug18e.ino
+++
@@ -1,15 +0,0 @@
-#include <Servo.h>
-Servo myservo; // 建立Servo物件，控制伺服馬達
-void setup() {  myservo.attach(3);}
-void loop()
-{   for (int i = 0; i <= 180; i += 1)
-  {
-        myservo.write(i);
-    delay(20);
-     
-  }  
-  for (int i = 180; i >= 0; i -= 1) 
-  {
-        myservo.write(i);
-  }
-}
